ab4c9d6163d125b2c4c6d127c4a5b4ec5622fede
Lxylona <lllinxueying@gmail.com>
2018-04-03 22:10:28 +0800

    个人页面数据参数已经定义好

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,11 +1,70 @@
 // pages/me/me.js
+
+var initData = {
+  // 页面状态： loading 加载中 error错误 me个人页面 modify编辑个人资料页面 
+  pageState: 'me',
+  userInfo: { // 用户信息
+    avatar_url: '', // 头像url，如果没有注册则为微信头像
+    name: '', // 姓名
+    city: '', // 城市
+    college: '', // 大学
+    compony: '', // 公司
+    isSectorMember: false, // 是否为校友会部门成员
+    bussinessCardUrl: '', // 名片url
+    followList: [1, 2, 3] // 关注的校友的ID
+  },
+  isShowBussnessCard: false, // 是否显示名片：名片以弹出层的形式出现
+  isShowAboutUs: false, // 是否显示“关于我们”模块 也是弹出层
+}
+
+var debugData = {
+  // 页面状态： loading 加载中 error错误 me个人页面 modify编辑个人资料页面 
+  pageState: 'me',
+  userInfo: { // 用户信息
+    avatar_url: '', // 头像url，如果没有注册则为微信头像
+    name: '', // 姓名
+    city: '', // 城市
+    college: '', // 大学
+    compony: '', // 公司
+    isSectorMember: false, // 是否为校友会部门成员
+    bussinessCardUrl: '', // 名片ur
+  },
+  isShowBussnessCard: false, // 是否显示名片：名片以弹出层的形式出现
+  isShowAboutUs: false, // 是否显示“关于我们”模块 也是弹出层
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-  
+  data: debugData,
+
+  // 事件处理函数
+  // 命名无能，恳请给出命名建议
+
+  logout: function () { //退出登陆
+    console.log('logout')
+  },
+
+  completeModify: function () { // 修改资料页面的【确定】
+    console.log('completeModify')
+  },
+
+  backToMe: function () { // 返回“我的“页面
+    console.log('backToMe')
+  },
+
+  jumpToSchoolmates: function () { // 跳转到“我的校友圈“页面
+    console.log('jumpToSchoolmates')
+  },
+
+  jumpToChatRoom: function () { // 跳转到聊天页面，目前我的理解是小程序自带的客服接口
+    console.log('jumpToChatRoom')
+  },
+
+  jumpToDepartment: function () { // 跳转到“我的部门”页面
+    console.log('jumpToDepartment')
   },
 
   /**
