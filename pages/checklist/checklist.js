05543bdaf46b56d7e42f7afd02b787d300cf700b
chencangui <1130285003@qq.com>
2019-03-30 11:07:01 +0800

    审核接口和快速审核功能

--- pages/checklist/checklist.js
+++ pages/checklist/checklist.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    proList: []
+    proList: [],
+    checkData: {},
+    radioItem:{}
   },
 
   /**
@@ -15,9 +17,6 @@ Page({
     wx.setNavigationBarTitle({
       title: '审核列表'
     }),
-    this.setData({
-        test: '01',
-      })
     this.getProList();
   },
 
@@ -69,10 +68,14 @@ Page({
   onShareAppMessage: function () {
 
   },
+
   getProList: function () {
     var self = this;
+    var sid = wx.getStorageSync('credentials');
+    // sid = "yiz:b996d73ec77be9743adbf83d0cbd832632c98151c6a68184e8b5861a3ac54597";
+    console.log("getProList ", sid)
     wx.request({
-      url: getApp().serverUrl + '/api/user/reviewlist/yiz:b996d73ec77be9743adbf83d0cbd832632c98151c6a68184e8b5861a3ac54597',
+      url: getApp().serverUrl + '/api/user/reviewlist/' + sid,
       method: 'GET',
       success: function (res) {
         console.log(res);
@@ -90,5 +93,100 @@ Page({
     wx.navigateTo({
       url: '../audit_form/audit_form?uid=' + e.target.dataset.uid
     });
-  }
+  },
+
+  checkSubmit: function (e) {
+    var data = e.target.dataset;
+    var status = data.status;
+    var checkItem = data.checkdata;
+    var radioItem = data.checkitem == 0 ? false : true;
+    var sid = wx.getStorageSync('credentials');
+    // sid = "yiz:b996d73ec77be9743adbf83d0cbd832632c98151c6a68184e8b5861a3ac54597";
+    console.log('checkSubmit ', radioItem);
+    if (status != "cancel") {
+      wx.showLoading({
+        title: '加载中',
+      })
+      wx.request({
+        url: getApp().serverUrl + '/api/user/review/' + sid + '/' + checkItem.uid,
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          comment: "优秀",
+          approved: radioItem,
+          uid: checkItem.uid,
+        },
+        success(res) {
+          console.log(res.data)
+          wx.hideLoading();
+          if (res.data.success) {
+            wx.showToast({
+              title: '上传成功',
+              icon: 'success',
+              duration: 4000
+            })
+            setTimeout(function () {
+
+            }, 4000)
+
+          } else {
+            this.failAlert("请求失败！");
+          }
+        },
+        fail() {
+          wx.hideLoading();
+          this.failAlert("请求失败！");
+        }
+      })
+    }
+  },
+
+  /**
+ * 弹窗
+ */
+  toQuickCheck: function (e) {
+    var checkData = e.target.dataset.check;
+    console.log('快速审核', checkData);
+    this.setData({
+      showModal: true,
+      checkData: checkData,
+    })
+  },
+
+  radioChange: function (e) {
+    var radioItem = e.detail.value;
+    this.setData({
+      showModal: true,
+      radioItem: radioItem,
+    })
+    console.log('radio发生change事件，携带value值为：', radioItem)
+  },
+  /**
+  * 弹出框蒙层截断touchmove事件
+  */
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+  /**
+   * 对话框确认按钮点击事件
+   */
+  onConfirm: function (e) {
+    this.hideModal();
+    this.checkSubmit(e);
+  },
 })
