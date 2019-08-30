5770d62700d76b760cb2cb36f04be187db20e3b6
linkvivian <2413624599@qq.com>
2019-08-30 11:53:43 +0800

    feat:增加咨询记录功能

--- pages/consult/add_msg/add_msg.js
+++ pages/consult/add_msg/add_msg.js
@@ -14,9 +14,18 @@ Page({
     inputValue: '',
     wxAppendData: [],
     submitTime: '',
+    pid: '',
+    name: '',
+    sessionOrAdress: '',
+    status: '',
     fromUid: '',
     toUid: '',
-    closeUid: ''
+    id: '',
+    consultList: '',
+    offset: 0,
+    isDone: '',
+    from: '', //我的咨询还是咨询我的来的
+    color: ''  //不同状态颜色
   },
   //留言的伸展与收起
   ellipsis: function () {
@@ -57,14 +66,15 @@ Page({
       wxAppendData
     })
 
+    
     util.send({
       url: '/api/lawyer/msg/add/' + sid,
       method: 'POST',
       data: {
         msg: self.data.inputValue,
-        fromUid: 392,
-        toUid: 605,
-        pid: 7
+        fromUid: self.data.toUid,
+        toUid: self.data.fromUid,
+        pid: self.data.pid
       },
       callback: function (res) {
         if(res.data.success){
@@ -76,26 +86,54 @@ Page({
     });
   },
   
+  judgeStatus(status) {
+    if(status === '已完成') {
+      return 'done'
+    }else{
+      return 'undone'
+    }
+  },
+
+  judgeColor(status) {
+   switch(status){
+     case '咨询中': return 'blue'; break;
+     case '已完成': return 'green'; break;
+     default: return 'gray';
+   }
+  },
+
+  //加载函数
   onLoad: function (option) {    
     console.log(option)
+    var self = this;
+    let { pid, lawyerName, lawyerAdress, lawyerStatus, from} = option;
+    this.setData({
+      pid,
+      name: lawyerName,
+      sessionOrAdress: lawyerAdress,
+      status: lawyerStatus,
+      isDone: self.judgeStatus(lawyerStatus),
+      from,
+      color: self.judgeColor(lawyerStatus)
+    })
+    // 获取微信服务凭证
     let sid = app.getCredentials();
 
-    var self = this;
-    // 获取微信服务凭证
-    //判断是否是律师，是的话不显示关闭咨询框
-    // util.send({
-    //   url: '/api/lawyer/is_lawyer/' + sid,
-    //   method: 'GET',
-    //   callback: function (res) {
-    //     var isLawyer = res.data.data;
-    //     self.setData({
-    //       isShow: !isLawyer
-    //     })
-    //   }
-    // });
-
+    // 判断是否是律师，是的话不显示关闭咨询框
     util.send({
-      url: '/api/lawyer/msg/' + sid + '/1',
+      url: '/api/lawyer/is_lawyer/' + sid,
+      method: 'GET',
+      callback: function (res) {
+        var isLawyer = res.data.data;
+        self.setData({
+          isShow: !isLawyer
+        })
+      }
+    });
+
+//获取当前留言
+    util.send({
+      url: '/api/lawyer/msg/' + sid + '/' + pid,
       method: 'GET',
       callback: function (res) {
         var consultList = res.data.data;
@@ -104,14 +142,72 @@ Page({
           currentMsg: consultList.top.msg,
           fromUid: consultList.top.fromUid,
           toUid: consultList.top.toUid,
-          closeUid: consultList.top.id
+        })
+      }
+    });
+  //获取留言列表
+  this.requestData();
+  },
+
+//分页请求列表数据
+  requestData() {
+    let sid = app.getCredentials();
+    var self = this;
+    var url = this.data.from === 'consute_me' ? 'consulting_me/' : 'my_consulting/';
+    util.send({
+      url: '/api/lawyer/' + url + sid,
+      method: 'GET',
+      data: {
+        offset: self.data.offset,
+        count: 10,
+        type: self.data.isDone
+      },
+      callback: function (res) {
+        var consultList = res.data.data;
+        console.log(consultList)
+        //请求到的数据为空时，关掉加载开关
+        if (consultList && consultList.length === 0) {
+          var flag = false;
+        } else {
+          var flag = true;
+        }
+        //连接
+        //改变返回数据的时间格式
+       if(consultList) {
+         //标记已读
+         util.send({
+           url: '/api/lawyer/msg/read/' + sid + '/' + pid,
+           method: 'POST',
+         });
+         consultList.map(item => {
+           item.gmtCreate = util.formatTime(new Date(item.gmtCreate), true)
+         })
+         consultList = self.data.consultList.concat(consultList)
+
+       }
+        var offset = self.data.offset + 4;
+        self.setData({
+          consultList,
+          offset,
+          flag
         })
       }
     });
   },
-
+  //出发加载更多
+  loadMore() {
+    if (this.data.flag) {
+      this.requestData();
+    }
+  },
 //获取输入框内容
   ins: function(e) {
+    if (e.detail.value.length === 300) {
+      wx.showToast({
+        title: '最多只能输入300字!',
+        icon: 'none'
+      })
+    }
     this.setData({
       inputValue: e.detail.value
     })
@@ -125,7 +221,7 @@ Page({
       method: 'POST',
       data: {
         finished: true,
-        id: self.closeUid
+        id: self.data.pid
       },
       callback: function (res) {
         console.log(res.data)
