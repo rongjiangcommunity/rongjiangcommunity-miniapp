a064483fb65adb1f81bdb9962ec2b80fec63b8f3
3117005290肖威 <shawve@163.com>
2019-09-29 09:59:39 +0800

    新增分页框架

--- pages/consult/consult.js
+++ pages/consult/consult.js
@@ -7,6 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    offset:0,
     lawyerList:[],
     isLawyer: false,
     infoPro:'刑事辩护及控告，企业刑事风险防护，刑民交叉案件和其他相关方面的案件'
@@ -17,22 +18,12 @@ Page({
    */
   onLoad: function (options) {
     const credentials = app.getCredentials();
+    this.getLawyerList()
     const that=this;
     this.getWindowHeight();
     wx.setNavigationBarTitle({
       title: '律师咨询'
     });
-    // 获取律师列表
-    wx.request({
-      url: app.serverUrl + '/api/lawyer/lawyers/' + credentials,
-      success(res){
-        if(res.data.success===true){
-          that.setData({
-            lawyerList:res.data.data,
-          });
-        }
-      }
-    })
     // 检测用户是否是律师
     wx.request({
       url: app.serverUrl + '/api/lawyer/is_lawyer/' + credentials,
@@ -73,8 +64,25 @@ Page({
       }
     })
   },
+ // 获取律师列表
+  getLawyerList:function(e){
+    let that = this;
+    const credentials = app.getCredentials();
+    wx.request({
+      url: app.serverUrl + '/api/lawyer/lawyers/' + credentials,
+      success(res) {
+        if (res.data.success === true) {
+          that.setData({
+            lawyerList: res.data.data,
+          });
+        }
+      }
+    })
+  },
   // 跳转至律师主页
   goToLawyer:function(e){
+    console.log("asdasdasdasd")
+    console.log(e.currentTarget.dataset.info)
     const info = JSON.stringify(e.currentTarget.dataset.info);
     wx.navigateTo({
       url: './lawyer_home/lawyer_home?info='+info,
@@ -107,6 +115,14 @@ Page({
         });
       }
     });
+  },
+  //分页触底操作
+  lowerMoreLawyers:function(e){
+    let that = this;
+    that.setData({
+      offset: that.data.offset + that.data.count,
+    })
+    that.getLawyerList(); //重新获取信息
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
